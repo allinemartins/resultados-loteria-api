a83edcb0da1367af0b6541570f634c837a9f41a5
Alline Martins <allinemartins@msn.com>
2024-01-04 21:13:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const bodyParser = require('body-parser');
 const porta = 8000;
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.json());
 
 //reescrever o arquivo json 
