f4e588010855ebaae5fdf02985982b998f3ef278
Alline Martins <allinemartins@msn.com>
2024-03-06 17:48:21 -0300

    changed the informations

--- informations.js
+++ informations.js
@@ -1,32 +1,38 @@
 let info = require('./data-files/informations.json');
+const path = require('path');
+const fs = require('fs');
 
 class Informations {
 
     setInformations(lastScriptCron) {
         info.lastScript = lastScriptCron;
         info.lastDatetime = new Date();
-        this.writeFile(info);        
+
+        // Criar um arquivo temporário
+        const tempFilePath = path.join(__dirname, 'data-files', 'informations_temp.json');
+        this.writeFile(tempFilePath, info);
+
+        // Mover o arquivo temporário para o local desejado
+        const finalFilePath = path.join(__dirname, 'data-files', 'informations.json');
+        fs.rename(tempFilePath, finalFilePath, err => {
+            if (err) throw err;
+            console.log(`Done writing in the informations`);
+        });
     }
 
     getInformations() {
         return info;
     }
 
-    writeFile(newObject) {
-        //reescrever o arquivo json 
-        const path = require('path');
-        const fs = require('fs');
-
-        const filePath = path.join(__dirname, 'data-files', 'informations.json');
-
+    writeFile(filePath, newObject) {
+        // Escrever no arquivo temporário
         fs.writeFile(filePath, JSON.stringify(newObject), err => {
-            // Checking for errors
             if (err) throw err;
-            console.log(`Done writing in the informations`); // Success
+            console.log(`Done writing in the temporary file`);
         });
     }
 
 }
 
-// Export the object
+// Exportar o objeto
 module.exports = Informations;
