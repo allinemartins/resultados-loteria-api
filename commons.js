26e048d2d3b710d5634de9c8d8a788c359731053
Alline Martins <allinemartins@msn.com>
2024-03-06 20:59:00 -0300

    change commons

--- commons.js
+++ commons.js
@@ -2,28 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const tmp = require('tmp');
 
-module.exports.writeFile = function(newObject, nmFile){
-
-    // Criar um arquivo temporário de forma segura
-    const tempFileInfo = tmp.fileSync({ postfix: '.json' });
+module.exports.writeFile = function(newObject, nmFile) {
+    // Criar um arquivo temporário de forma segura na pasta temporária
+    const tempFileInfo = tmp.fileSync({ postfix: '.json', dir: '/tmp' });
     const tempFilePath = tempFileInfo.name;
 
     // Escrever no arquivo temporário
     fs.writeFileSync(tempFilePath, JSON.stringify(newObject));
     console.log(`Done writing in the temporary file`);
 
-    // Copiar o conteúdo do arquivo temporário para o destino
+    // Mover o arquivo temporário para a pasta original
     const finalFilePath = path.join(__dirname, 'data-files', `${nmFile}.json`);
+    fs.renameSync(tempFilePath, finalFilePath);
 
-    const readStream = fs.createReadStream(tempFilePath);
-    const writeStream = fs.createWriteStream(finalFilePath);
-
-    readStream.pipe(writeStream);
-
-    writeStream.on('finish', () => {
-        // Limpar o arquivo temporário se necessário
-        tempFileInfo.removeCallback();
-
-        console.log(`Done writing in the ${nmFile}`);
-    });
+    // Limpar o arquivo temporário
+    tempFileInfo.removeCallback();
+    console.log(`Done writing in the ${nmFile}`);
 }
