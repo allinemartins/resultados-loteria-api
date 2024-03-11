978adddc000e492f1d275e14b9529937f36956c9
Alline Martins <allinemartins@msn.com>
2024-03-11 15:11:03 -0300

    remove jobs

--- routes/cron.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const jobs = require('../controllers/jobs.controller');
-
-router.get("/", jobs.getJob);
-
-module.exports = router
