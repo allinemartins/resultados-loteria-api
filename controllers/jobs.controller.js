c790b04bfc25c924b22a02a95ae3ad4bde1ee3ce
Alline Martins <allinemartins@msn.com>
2024-03-11 16:32:33 -0300

    remove files

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -1,11 +1,11 @@
 const jobsController = {
     mainJob: async (request, res) => {
         try {
-            /*const authHeader = request.headers.get('Authorization');
+            const authHeader = request.headers.get('Authorization');
 
             if (!authHeader || authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
                 return res.status(401).json({ error: 'Unauthorized' });
-            }*/
+            }
             const data = `Hello Cron Job, Data: ${new Date()}`;
             console.error(data);
             return res.status(200).json({ success: true, data: data });
--- src/app/api/cron/route.js
+++
@@ -1,7 +0,0 @@
-//const fetch = require('node-fetch');
-
-async function GET(request) {
-  
-}
-
-//module.exports = GET;
