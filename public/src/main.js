aa855258407cb79c40921635949828c4721e6011
Alline Martins <allinemartins@msn.com>
2024-03-15 10:00:18 -0300

    Update main.js

--- public/src/main.js
+++ public/src/main.js
@@ -15,3 +15,30 @@ function redirectWithBaseUrl(path) {
     var fullUrl = baseUrl + path;
     window.location.href = fullUrl;
 }
+
+//teste
+// Definindo a URL da API
+const apiUrl = 'https://servicebus2.caixa.gov.br/portaldeloterias/api/lotofacil/3050';
+
+// Fazendo a solicitação GET usando o método fetch
+fetch(apiUrl)
+  .then(response => {
+    // Verificando se a resposta da solicitação é bem-sucedida (status 200)
+    if (response.ok) {
+      // Convertendo a resposta para JSON
+      return response.json();
+    }
+    // Caso contrário, lançando um erro
+    throw new Error('Erro ao fazer a solicitação GET');
+  })
+  .then(data => {
+    // Manipulando os dados obtidos da API
+    console.log('Dados recebidos:', data);
+  })
+  .catch(error => {
+    // Capturando e tratando erros
+    console.error('Ocorreu um erro:', error);
+  });
+
+
+
