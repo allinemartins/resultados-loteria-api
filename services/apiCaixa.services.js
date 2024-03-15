f55d5bb6503bdfdf616c21a6c185bec122834d4c
Alline Martins <allinemartins@msn.com>
2024-03-15 09:38:48 -0300

    Update apiCaixa.services.js

--- services/apiCaixa.services.js
+++ services/apiCaixa.services.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const https = require('https');
+//const https = require('https');
 
 class ApiCaixa {
 
@@ -9,13 +9,10 @@ class ApiCaixa {
 
     async getData() {
         try {
-            const agent = new https.Agent({
+            /*const agent = new https.Agent({
                 rejectUnauthorized: false, // Configuration to accept untrusted certificates
-            });
-            const resposta = await axios.get(this.apiRequest, { httpsAgent: agent });
-            console.log('Response status:', resposta.status);
-            console.log('Response headers:', resposta.headers);
-            console.log('Response data:', resposta.data);
+            });*/
+            const resposta = await axios.get(this.apiRequest);            
             return resposta.data;
         } catch (erro) {
             console.error('Error in the made request:', `${erro.message} ${this.apiRequest}`);
